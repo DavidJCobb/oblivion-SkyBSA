ddfb88cee1f74e4a26a79d1fc5947fafbc2ed13d
DavidJCobb <831497+DavidJCobb@users.noreply.github.com>
2020-03-03 04:40:21 +0100

    Create CheckFileIsOverridden.h

---
+++ research/CheckFileIsOverridden.h
@@ -0,0 +1,34 @@
+bool Archive::CheckFileIsOverridden(BSAEntry& file, const char* looseFilePath) {
+   if (!INI:Archive:bInvalidateOlderFiles)
+      return false;
+   if (file.offset & 0x80000000) // high bit is used as an "already checked" flag
+      return false;
+   if (looseFilePath) {
+      char[MAX_PATH] finalPath = "Data\\"; // at esp40
+      auto eax = strlen(looseFilePath); // inlined
+      // Prepend "Data\\" to the loose file path:
+      memcpy(finalPath + strlen(finalPath), looseFilePath, eax);
+      //
+      struct _stat esp10;
+      auto eax = _stat(finalPath, &esp10);
+      if (eax != -1) {
+         //
+         // The (_stat) call managed to find a matching loose file 
+         // and retrieved some basic information about it, including 
+         // its timestamp.
+         //
+         // this->myDateModified is stored in registers edi and esi
+         //
+         // esp10.unk24 is checked one register at a time, using ecx
+         //
+         // timestamp type is __time64_t
+         //
+         if (esp10.unk20 > this->myDateModified) {
+            file.offset &= 0x80000000; // a file with a zero offset is "invalid"
+            return true;
+         }
+      }
+   }
+   file.offset |= 0x80000000; // mark the file as already checked so we can skip the above next time
+   return false;
+}
